c04b609f1d72db7a6a2d212debcee31372b8cd44
Abraham Arce <xe1gyq@gmail.com>
2018-02-18 03:38:44 +0000

    Update
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ Thing/Data/Actuators/Actuators.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import psutil
+import signal
+import sys
+import time
+
+def ThingDataSensors():
+    netdata = psutil.net_io_counters()
+    data = netdata.packets_sent + netdata.packets_recv
+    return data
+
+def ThingDataActuators(status):
+    print "Thing Data Actuators %s" % status
+
+def SignalHandler(signal, frame):
+    sys.exit(0)
+
+if __name__ == '__main__':
+
+    signal.signal(signal.SIGINT, SignalHandler)
+
+    while True:
+        print "Thing Data Sensors: %s " % ThingDataSensors()
+        time.sleep(5)
+
+# End of File
